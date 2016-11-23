e5029f543f4bc9198aaa05b55565b1a4b6016880
rufeer <rufeer@outlook.com>
2016-11-23 21:34:14 +0800

    Update the Answer.py      ------  40%

--- Answer.py
+++ Answer.py
@@ -12,7 +12,8 @@ user_agent = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) "
               "AppleWebKit/537.36 (KHTML, like Gecko) "
               "Chrome/36.0.1985.143 Safari/537.36")
 def signin(username, password):
-    login_url  = 'http://10.13.54.81/'
+    login_url  = 'http://10.13.54.81/index.php'
+    # login_url = 'http://10.13.54.81/login/nsindex_student.php'
     login_data = {
         'username': username,
         'password': password,
@@ -21,18 +22,37 @@ def signin(username, password):
         'User-Agent': user_agent,
         'Referer': 'http://10.13.54.81/',
     }
-
-    try:
-        login_res = English_session.post(login_url, data=login_data, headers=post_headers)
-    except requests.exceptions.RequestException, e:
-        print "Post failed...\n", e
-        sys.exit(2)
-
+    login_res = English_session.post(login_url, data=login_data, headers=post_headers)
     if login_res.status_code == 200:
         print "Login Successfully!"
     else:
         print "Login failed..."
-        sys.exit(2)
+
+def answer(target_url, UnitID, KidID, ItemID, Answer):
+    SectionID = target_url[33]
+    SisterID  = target_url[34]
+    TestID    = SectionID + '.' + SisterID
+    ans = 'Item_' + ItemID
+    answer_data = {
+        'UnitID': UnitID,
+        'SectionID': SectionID,
+        'SisterID': SisterID,
+        'TestID': TestID,
+        'KidID': KidID,
+        'ItemID': ItemID,
+        ans: Answer,
+    }
+    post_headers = {
+        'User-Agent': user_agent,
+        'Referer': 'http://10.13.54.81/book/book25/dj21.php',
+    }
+    cookies = {
+        'Hm_lvt_8a1d0cf914523c7ed112dbd25e018957': '1479901106',
+        'NHCELoginCounter': '1',
+    }
+    url_response = English_session.post(target_url, data=answer_data, headers=post_headers, cookies=cookies)
+    print English_session.cookies
+    print url_response.content
 
 
 
@@ -41,9 +61,10 @@ def signin(username, password):
 
 if __name__ == "__main__":
     English_session = requests.Session()
-    username = raw_input("请输入用户名: ")
+    # username = raw_input("请输入用户名: ")
+    username = '2015302407'
     # password = getpass.getpass()
-    password = raw_input("请输入密码: ")
+    # password = raw_input("请输入密码: ")
+    password = 'nhce111'
     signin(username, password)
-
-    book_url = 'http://10.13.54.81/book/index.php'
+    answer('http://10.13.54.81/book/book25/dj21.php', '7', '1', '245', 'C')

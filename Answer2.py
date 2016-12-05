49eb19df5b8fbf944107f3dcb8573b76e871568b
rufeer <rufeer@outlook.com>
2016-12-05 17:36:24 +0800

    STSJC3 6-10 completed

--- Answer2.py
+++ Answer2.py
@@ -52,16 +52,15 @@ def answer(BookID):
     for line in lines:
         if line == '' or line[0] == '#':
             continue
-        content   = line.split()
-        URL       = content[0]
+        content   = line.split('##')
+        URL       = content[0].strip()
         UnitID    = content[1]
-        KidID     = content[2]
-        ItemID    = content[3]
-        Answer    = content[4]
-        SectionID = URL[33]
-        SisterID  = URL[34]
-        TestID    = SectionID + '.' + SisterID
-        Item_N    = 'Item_' + ItemID
+        TestID    = content[2]
+        SectionID = TestID[0]
+        SisterID  = TestID[2]
+        KidID     = content[3]
+        ItemID    = content[4]
+        Answer    = content[5]
 
         payload = {
             'whichAction': 'checkMyProgress',
@@ -72,7 +71,7 @@ def answer(BookID):
             'User-Agent': user_agent,
             'Referer': URL,
         }
-        English_session.post(main_url + "/book/book%d/jdls3ajax.php" % BookID, data=payload)
+        English_session.post(main_url + "/book/book%d/nslsajax.php" % BookID, data=payload)
         url = main_url + "/book/book%d/unit_index.php?UnitID=" % BookID + UnitID
         English_session.get(url)
         base_data = {
@@ -80,62 +79,10 @@ def answer(BookID):
             'SectionID': SectionID,
             'SisterID': SisterID,
             'TestID': TestID,
-            'KidID': '1',
+            'KidID': KidID,
+            'ItemID': ItemID,
         }
-        if TestID == '2.1':
-            answer_data = dict(base_data.items()+{
-                'KidID': KidID,
-                'ItemID': ItemID,
-                Item_N: Answer,
-            }.items())
-        elif TestID == '2.2' or TestID == '2.3':
-            item = [('Item_'+ItemID)]*5
-            for i in range(1,5):
-                item[i] = 'Item_' + str(int(ItemID)+i)
-            answer_data = dict(base_data.items()+{
-                'ItemID': '',
-                item[0]: Answer[0],
-                item[1]: Answer[1],
-                item[2]: Answer[2],
-                item[3]: Answer[3],
-                item[4]: Answer[4],
-            }.items())
-        elif TestID == '2.4':
-            answer = line.find('answer:')
-            answer = line[answer+7:].split('#')
-            answer_data = dict(base_data.items()+{
-                'ItemID': ItemID,
-                'Item_0':answer[0],
-                'Item_1': answer[1],
-                'Item_2': answer[2],
-                'Item_3': answer[3],
-                'Item_4': answer[4],
-            }.items())
-        elif TestID == '5.1' or TestID == '5.2':
-            answer_data = dict(base_data.items()+{
-                'ItemID': ItemID,
-                'Item_1': Answer[0],
-                'Item_2': Answer[1],
-                'Item_3': Answer[2],
-                'Item_4': Answer[3],
-                'Item_5': Answer[4],
-            }.items())
-        elif TestID == '5.3':
-            answer = line.find('answer:')
-            answer = line[answer + 7:].split('#')
-            answer_data = dict(base_data.items()+{
-                'ItemID': ItemID,
-                'Item_0': answer[0],
-                'Item_1': answer[1],
-                'Item_2': answer[2],
-                'Item_3': answer[3],
-                'Item_4': answer[4],
-                'Item_5': answer[5],
-                'Item_6': answer[6],
-                'Item_7': answer[7],
-                'Item_8': answer[8],
-                'Item_9': answer[9],
-            }.items())
+        answer_data = dict(base_data.items()+eval(Answer).items())
 
         # Submit answers
         English_session.post(URL, data=answer_data, headers=post_headers)

01d50de6f46aa560dd3e14e4724a3692ae013965
Rufeer <rufeer@outlook.com>
2017-06-01 21:55:46 +0800

    Update TSJC4 & STSJC4

--- Resources/param2dic.py
+++ Resources/param2dic.py
@@ -4,7 +4,11 @@ while(1):
     ans   = {}
     for i in item:
         key, data = i.split('=')
-        ans[key] = data.replace('+', ' ')
+        ans[key] = data.replace('+', ' ').replace('%5E', '^')
     print ans
 
-
+# while(1):
+#     num = int(raw_input())
+#     str = raw_input()
+#     for i in range(10):
+#         print "{'Item_%d': '%s'}" % (num+i, str[i])
